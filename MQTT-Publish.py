7ee6bc8511dab29a5e9e7898b783b7246ff64086
Les Pounder <lespounder@gmail.com>
2017-08-08 08:12:55 +0100

    Updated for Summer Code Camp

--- MQTT-Publish.py
+++ MQTT-Publish.py
@@ -1,11 +1,9 @@
 import time
 import paho.mqtt.client as mqtt
-from w1thermsensor import W1ThermSensor
-sensor = W1ThermSensor()
 client = mqtt.Client()
-client.connect("192.168.1.147", 1883, 60)
+client.connect("BROKER IP ADDRESS", 1883, 60)
 while True:
-    temp = sensor.get_temperature()
+    temp = "WHERE IS THE TEMPERATURE?"
     client.publish("temperature",temp)
     print(temp)
     time.sleep(10)
--- MQTT-Subscriber.py
+++ MQTT-Subscriber.py
@@ -1,9 +1,4 @@
 import paho.mqtt.client as mqtt
-from gpiozero import LED
-
-red = LED(17)
-
-
 
 def on_connect(client, userdata, flags, rc):  
         print("Connected with result code "+str(rc))
@@ -14,20 +9,16 @@ def on_message(client, userdata, msg):
         message = message[2:(len(message)-1)]
         message = float(message)
         print(message)
-        if message < 10.0 and message >= 0.0:
-                print(message)
-                red.blink(1,1)
-        elif message >= 10.0 and message < 20:
-                print(message)
-                red.blink(0.5,0.5)
-        elif message >=20 and message <30:
-                print(message)
-                red.blink(0.25,0.25)
-        
+"""
+        if 
+        elif 
+        elif 
+        else:
+"""
 
 client = mqtt.Client()  
 client.on_connect = on_connect  
 client.on_message = on_message  
-client.connect("192.168.1.147", 1883, 60)
+client.connect("BROKER IP ADDRESS", 1883, 60)
 
 client.loop_forever()  
